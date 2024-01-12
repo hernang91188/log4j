0d8adb75dd8bec6c2145d43d88340684d9b5e1eb
hernang9188@gmail.com <hernang9188@gmail.com>
2024-01-12 17:08:10 -0300

    commit con multples logs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,15 @@ package org.example;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 public class Main {
-    private static final Logger logger = LogManager.getLogger();
-
+    private static final Logger logger = LogManager.getLogger(Main.class);
+    private static final Logger fileLogger1 = LogManager.getLogger("FileLogger1");
+    private static final Logger fileLogger2 = LogManager.getLogger("FileLogger2");
     public static void main(String[] args) {
 
         for (int i = 0; i <8000 ; i++) {
             logger.info("Hello, Log4j!");
-
+            fileLogger1.info("Hello, app1!");
+            fileLogger2.info("Hello, app2j!");
         }
         logger.info("Hello, Log4j!");
         logger.error("This is an error message.");
